a34b9d9b0cc8ee3b0d929043a834ee9241087c32
Nvm-seff <Saifullah.riz@gmail.com>
2026-04-26 11:58:30 +0500

    Disabled copying in RAII

--- include/ffmpeg_raii.hpp
+++ include/ffmpeg_raii.hpp
@@ -3,16 +3,31 @@
 extern "C" {
 #include <libavformat/avformat.h>
 #include <libavcodec/avcodec.h>
+#include <libavutil/avutil.h>
 }
+
+// Macro to quickly disable copying for RAII structs
+#define DISABLE_COPY_AND_MOVE(Classname) \
+    Classname(const Classname&) = delete; \
+    Classname& operator=(const Classname&) = delete; \
+    Classname(Classname&&) = delete; \
+    Classname& operator=(Classname&&) = delete;
 
 struct FormatContext {
     AVFormatContext* ctx = nullptr;
 
+    FormatContext() {
+        ctx = avformat_alloc_context();
+    }
+
     ~FormatContext() {
         if (ctx) {
+            // avformat_close_input also calls avformat_free_context
             avformat_close_input(&ctx);
         }
     }
+
+    DISABLE_COPY_AND_MOVE(FormatContext)
 };
 
 struct Packet {
@@ -28,7 +43,41 @@ struct Packet {
         }
     }
 
+    // AVPacket requires frequent unreferencing in loops
     void unref() {
         av_packet_unref(pkt);
     }
+
+    DISABLE_COPY_AND_MOVE(Packet)
+};
+
+struct CodecContext {
+    AVCodecContext* ctx = nullptr;
+
+    // Usually initialized via avcodec_alloc_context3(codec)
+    explicit CodecContext(AVCodecContext* c) : ctx(c) {}
+
+    ~CodecContext() {
+        if (ctx) {
+            avcodec_free_context(&ctx);
+        }
+    }
+
+    DISABLE_COPY_AND_MOVE(CodecContext)
+};
+
+struct Frame {
+    AVFrame* frame = nullptr;
+
+    Frame() {
+        frame = av_frame_alloc();
+    }
+
+    ~Frame() {
+        if (frame) {
+            av_frame_free(&frame);
+        }
+    }
+
+    DISABLE_COPY_AND_MOVE(Frame)
 };
